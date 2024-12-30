95905c71f1d3e7d78a7da1eea1693ea53461ca84
Aymerickbou <112753724+Aymerickbou@users.noreply.github.com>
2024-12-30 21:09:29 +0100

    Update unite.java

--- unite.java
+++ unite.java
@@ -1,4 +1,19 @@
-public class unite {
-    private String nom;
-    private Integer coutEnPoints;
+abstract class unite {
+    private String name;
+    private int cost;
+
+    public unite(String name, int cost) {
+        this.name = name;
+        this.cost = cost;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public abstract void print();
 }
