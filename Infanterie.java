648570523c02b2766f045c8a471f816378c63c7f
Aymerickbou <112753724+Aymerickbou@users.noreply.github.com>
2024-12-30 21:08:29 +0100

    Update Infanterie.java

--- Infanterie.java
+++ Infanterie.java
@@ -1,3 +1,13 @@
-public class Infanterie extends unite{
-    private TypeUnite type;
+class Infantry extends unite {
+    private String type;
+
+    public Infantry(String name, int cost, String type) {
+        super(name, cost);
+        this.type = type;
+    }
+
+    @Override
+    public void print() {
+        System.out.println("Infanterie - Type : " + type + " - Nom : " + getName() + " (" + getCost() + " pts)");
+    }
 }
