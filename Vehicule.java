b3815949e6c6d84fa7e72a09e9952763b51fb8b4
Aymerickbou <112753724+Aymerickbou@users.noreply.github.com>
2024-12-30 21:08:53 +0100

    Update Vehicule.java

--- Vehicule.java
+++ Vehicule.java
@@ -1,3 +1,15 @@
-public class Vehicule extends unite{
+class Vehicle extends unite {
+    private String vehicleType;
+    private int transportCapacity;
 
+    public Vehicle(String name, int cost, String vehicleType, int transportCapacity) {
+        super(name, cost);
+        this.vehicleType = vehicleType;
+        this.transportCapacity = transportCapacity;
+    }
+
+    @Override
+    public void print() {
+        System.out.println("Véhicule - Type : " + vehicleType + " - Nom : " + getName() + " (" + getCost() + " pts, Capacité : " + transportCapacity + ")");
+    }
 }
