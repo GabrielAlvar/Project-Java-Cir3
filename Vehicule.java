e0a8bd71167e07d53385b843c468a2dfb94feee8
Aymerickbou <112753724+Aymerickbou@users.noreply.github.com>
2025-01-05 10:49:39 +0100

    Update Vehicule.java

--- Vehicule.java
+++ Vehicule.java
@@ -1,15 +1,19 @@
+// Classe qui représente une unité de véhicule, héritée de la classe abstraite 'unite'
 class Vehicule extends unite {
-    private String vehicleType;
-    private int transportCapacity;
+    private String vehicleType; // Type de véhicule (Transport ou Attaque)
+    private int transportCapacity; // Capacité de transport du véhicule (pertinent pour les véhicules de type Transport)
 
+    // Constructeur de vehicule
     public Vehicule(String name, int cost, String vehicleType, int transportCapacity) {
-        super(name, cost);
-        this.vehicleType = vehicleType;
-        this.transportCapacity = transportCapacity;
+        super(name, cost); // Appel au constructeur de la classe 'unite' pour initialiser le nom et le coût
+        this.vehicleType = vehicleType; // Initialisation du type de véhicule
+        this.transportCapacity = transportCapacity; // Initialisation de la capacité de transport
     }
+
 
     @Override
     public void print() {
+        // Affichage des détails du véhicule (type, nom, coût et capacité de transport)
         System.out.println("Véhicule - Type : " + vehicleType + " - Nom : " + getName() + " (" + getCost() + " pts, Capacité : " + transportCapacity + ")");
     }
 }
