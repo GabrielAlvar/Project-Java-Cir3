8760fde1215b86c574228a5c753a9a6916b34634
Aymerickbou <112753724+Aymerickbou@users.noreply.github.com>
2025-01-05 10:48:58 +0100

    Create Infantery.java

---
+++ Infantery.java
@@ -0,0 +1,17 @@
+// Classe infanterie, héritée de la classe abstraite 'unite'
+class Infantry extends unite {
+    private String type; // Type de l'infanterie (Soldat, Lourd, Spécial, Chef, etc.)
+
+    // Constructeur pour initialiser l'infanterie avec un nom, un coût et un type
+    public Infantry(String name, int cost, String type) {
+        super(name, cost); // Appel au constructeur de la classe 'unite' pour initialiser le nom et le coût
+        this.type = type; // Initialisation du type d'infanterie
+    }
+
+
+    @Override
+    public void print() {
+        // Affichage des détails de l'infanterie (type, nom et coût)
+        System.out.println("Infanterie - Type : " + type + " - Nom : " + getName() + " (" + getCost() + " pts)");
+    }
+}
