919c61b98d9c4a2bae1e86dda9bc48145a412794
Aymerickbou <112753724+Aymerickbou@users.noreply.github.com>
2025-01-05 10:50:42 +0100

    Update unite.java

--- unite.java
+++ unite.java
@@ -1,19 +1,24 @@
+// classe unite
 abstract class unite {
-    private String name;
-    private int cost;
+    private String name; // Nom de l'unité
+    private int cost; // Coût de l'unité en points
 
+    // Constructeur pour initialiser les attributs de l'unité
     public unite(String name, int cost) {
         this.name = name;
         this.cost = cost;
     }
 
+    //obtenir le nom de l'unité
     public String getName() {
         return name;
     }
 
+    //obtenir le coût de l'unité
     public int getCost() {
         return cost;
     }
+
 
     public abstract void print();
 }
